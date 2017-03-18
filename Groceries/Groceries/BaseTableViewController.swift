3814849f8542e07c6c5b7da6fe661513cd9a72d9
Greyson Wright <greysonmwright@gmail.com>
2017-03-17 20:40:02 -0500

    - added depot

--- Groceries/Groceries/BaseTableViewController.swift
+++ Groceries/Groceries/BaseTableViewController.swift
@@ -49,13 +49,6 @@ class BaseTableViewController: UITableViewController {
 		return sectionTitles![section]
 	}
 	
-//	override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//		let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 20))
-//		let titleLabel = UILabel(frame: view.frame)
-//		titleLabel.text = sectionTitles![section]
-//		return view
-//	}
-	
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		guard let rowContent = self.rowContent else {
 			return 0
@@ -78,6 +71,10 @@ class BaseTableViewController: UITableViewController {
 		return cell
     }
 
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.deselectRow(at: indexPath, animated: true)
+	}
+	
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
---
+++ Groceries/Groceries/Depot/Depot.swift
@@ -0,0 +1,91 @@
+//Copyright (c) 2012 The Board of Trustees of The University of Alabama
+//All rights reserved.
+//    
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
+//
+//1. Redistributions of source code must retain the above copyright
+//notice, this list of conditions and the following disclaimer.
+//2. Redistributions in binary form must reproduce the above copyright
+//notice, this list of conditions and the following disclaimer in the
+//documentation and/or other materials provided with the distribution.
+//3. Neither the name of the University nor the names of the contributors
+//may be used to endorse or promote products derived from this software
+//without specific prior written permission.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+//THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+//INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+//OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import Foundation
+
+enum DataSources {
+    case TestData
+    case Webservice
+    case LocalDB
+    case None
+}
+
+
+//MARK: Protocol
+@objc protocol DepotInterface{
+    func getString() -> String
+    func getAsyncString(name: String, response: @escaping ((String) -> Void))
+}
+
+//MARK: Class
+private let _shared = DepotSingleton()
+
+public class DepotSingleton {
+    // Singleton Access
+    class var shared: DepotSingleton {
+        return _shared
+    }
+    
+    // Variables
+    private var dataSourceType: DataSources = DataSources.None
+    private var dataSource: DepotInterface?
+    
+    // Init and Init Helpers
+	fileprivate init() {
+        self.dataSource = dataSourceForType(source: self.dataSourceType)
+    }
+	
+	// Datasource
+    func dataSourceForType(source: DataSources) -> DepotInterface?{
+        switch source {
+        case DataSources.TestData:
+            return TestData.shared
+        case DataSources.LocalDB:
+            return LocalDBData.shared
+        case DataSources.Webservice:
+            return WebserviceData.shared
+        default:
+            return nil
+        }
+    }
+    
+    func setNewDataSource(source: DataSources) {
+        self.dataSourceType = source
+        self.dataSource = dataSourceForType(source: source)
+    }
+    
+    //MARK: Callbacks
+    func getString() -> String? {
+        return self.dataSource?.getString()
+    }
+    
+    func getAsyncString(name: String, response: @escaping ((String) -> Void)) {
+        return self.dataSource!.getAsyncString(name: name, response: response)
+    }
+}
