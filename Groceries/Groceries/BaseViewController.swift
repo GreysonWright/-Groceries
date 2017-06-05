b3fb6b3350e503387ef0329769e1537b64d5b261
Greyson Wright <greysonmwright@gmail.com>
2017-06-04 21:34:27 -0500

    - deleted a few things

--- Groceries/Groceries/BaseViewController.swift
+++ Groceries/Groceries/BaseViewController.swift
@@ -12,31 +12,12 @@ import EasyPeasy
 class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-	
-		configureNavBar()
+		
+		navigationItem.title = nil
     }
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		
-	}
-	
-	// Unfortunately this is the cleanest way i could figure out how to do this
-	func configureNavBar() {
-		navigationItem.title = ""
-//		let titleView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 86))
-//		titleView.backgroundColor = UIColor.navBarBackground
-//		navigationController?.navigationBar.addSubview(titleView)
-//		titleView <- Edges(0)
-//		(navigationController?.navigationBar as! FatNavigationBar).backgroundView = titleView
-		
-//		let label = UILabel()
-//		label.frame.origin = CGPoint(x: 0, y: 0)
-//		label.font = UIFont.boldSystemFont(ofSize: 40)
-//		label.text = title
-//		label.sizeToFit()
-//		navigationController?.navigationBar.addSubview(label)
-//		label <- [Leading(15), Top(0)]
-//		(navigationController?.navigationBar as! FatNavigationBar).titleView = label
 	}
 }
